cb99c3857ced7c0992d5c87a688774759bb4a0c6
Cyril <cyril_ollier@yahoo.fr>
2017-10-20 18:59:52 +0200

    Les tableaux a N dimension

--- src/lanceur/lanceur.java
+++ src/lanceur/lanceur.java
@@ -4,98 +4,99 @@ import java.util.Scanner;
 
 public class lanceur {
 	
-	public static String[] tab1 = {"Tu as", "ans"};
-	//public static String[] tab1 = new String [2];	
-	public static int age;
-	public static String nom;
+	public static String dateBuilder[][] = {
+		{"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"},
+		{"", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimmanche"}
+	};
+	public static int jour;
+	public static int mois;
+	public static int numeroJour;	
 	
+	public static final int MOIS31 = 31; 
+	public static final int MOIS30 = 30;
+	public static int cpt31 = 1;
+	public static int cpt30 = 1;
+	
+	public static int semaineNum = 0;
+	public static int resteNbJour = 0;
+	public static String affiche;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
-	demande_info();
-	//tableau_simple();
-	//lecture_tableau();
-	//lecture_tableau_foreach();
-	ternaire();
-	ternaire_imbrique();
-	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez taper un numero de jour ?");
+		numeroJour = sc.nextInt();
 		
-	
-	
- }
-	
-	public static void demande_info()
-	{
-		Scanner sc = new Scanner(System.in)	;
-		System.out.println("quel est ton nom ?");
-		nom = sc.nextLine();
-		System.out.println("Salut " + nom);
-		System.out.println("quel est ton age ?");
-		age = sc.nextInt();
-	}
-	public static void tableau_simple()
-	{
-		// insertion donnee tableau
-		tab1[0] = "Tu as"; 
-		tab1[1] = "ans";
-		// fin
+		
+		
+		
+		if (numeroJour <= (MOIS31 * cpt31)) mois = 0; // janvier
+		else if (numeroJour <= (MOIS31 * cpt31) + (MOIS30 * cpt30) - 1) mois = 1; // fevrier
+		else if (numeroJour <= (MOIS31 * ++cpt31) + (MOIS30 * cpt30) - 1) mois = 2; //mars
+		else if (numeroJour <= (MOIS31 * cpt31) + (MOIS30 * ++cpt30) - 1) mois = 3; // avril
+		else if (numeroJour <= (MOIS31 * ++cpt31) + (MOIS30 * cpt30) - 1) mois = 4; // mai 
+		else if (numeroJour <= (MOIS31 * cpt31) + (MOIS30 * ++cpt30) - 1) mois = 5; // juin
+		else if (numeroJour <= (MOIS31 * ++cpt31) + (MOIS30 * cpt30) - 1) mois = 6; // juillet
+		else if (numeroJour <= (MOIS31 * ++cpt31) + (MOIS30 * cpt30) - 1) mois = 7; // aout
+		else if (numeroJour <= (MOIS31 * cpt31) + (MOIS30 * ++cpt30) - 1) mois = 8; // septembre
+		else if (numeroJour <= (MOIS31 * ++cpt31) + (MOIS30 * cpt30) - 1) mois = 9; // octobre
+		else if (numeroJour <= (MOIS31 * cpt31) + (MOIS30 * ++cpt30) - 1) mois = 10; // novembre
+		else if (numeroJour <= (MOIS31 * ++cpt31) + (MOIS30 * cpt30) - 1) mois = 11; // decembre
+		
+		//System.out.println("nous seron au mois de : " + dateBuilder[0][mois] + mois);
+		System.out.println("nous seron au mois de : " + dateBuilder[0][mois]);
+		
+		
+		
 			
-		System.out.println(tab1[0] + " " + age + " " + tab1[1]);
-	}
-	
-	public static void lecture_tableau()
-	{
-		for (int i= 0; i< tab1.length; i++)
+		int modulo = numeroJour % 7;
+		jour = modulo  == 0 ? 7 : modulo;
+		
+		//semaineNum = numeroJour / 7 ; //trouver le numero de la semaine
+		/*jour = numeroJour - semaineNum * 7 ;
+		if (jour == 0)
 		{
-			if (i != 1) // different de
-			//if (i%2 == 0) // avec modulo
-			{
-				System.out.print(tab1[i] +" " + age + " ");
-			}		
-			else 
-			{
-				System.out.print(tab1[i]);
-			}
-			
+			affiche = dateBuilder[1][6];
 		}
-	}
-	
-	public static void lecture_tableau_foreach()
-	{
-		for (String str : tab1)
+		else
 		{
-			if (str.equals(tab1[tab1.length-1]))
-					{
-				System.out.print(" "+ age +" "+ str +   " " );
-					}
-			else
-			{
-				System.out.print(str);
-			}
-		}
-	}
-	
-	public static void ternaire()
-	{
-		// condition ternaire
-		for (int i = 0; i < tab1.length; i++)
+			affiche = dateBuilder[1][jour-1];
+		}*/
+		/*if (jour == 0 )
 		{
-		String resultat = i !=1 ? tab1[i] +" " + age + " " : tab1[i];
-		System.out.print (resultat);
-		}
-		//fin 
+			jour = 1;
+		}*/
+		
+		//System.out.println("ce sera la semaine : " + semaineNum + " resultat " + semaineNum * 7 + " il reste en jour : " + jour + " le jour de la semaine est : " + affiche  );
+		//System.out.println("ce sera la semaine : " + semaineNum + " resultat " + semaineNum * 7 + " il reste : " + resteNbJour + " le jour de la semaine est : " + dateBuilder[1][jour-1]  );
+		//System.out.println("ce sera le : " + dateBuilder[1][jour-1]  );
+		System.out.println("ce sera un : " + dateBuilder[1][jour] );
+		//System.out.println("le numero de semaine est le : " + semaineNum );
+		
+		/*
+		switch(numeroJour)
+		{		
+		case 31 : mois = numeroJour <= 31; System.out.println(dateBuilder[0][0]); break;
+		case 60 : mois = numeroJour <= 31+29; System.out.println(dateBuilder[0][1]); break;
+		case 91 : mois = numeroJour <= (31*2)+29; break;
+		case 121 : mois = numeroJour <= (31*2)+30+29; break;
+		case 152 : mois = numeroJour <= (31*3)+30+29; break;
+		case 182 : mois = numeroJour <= (31*3)+(30*2)+29; break;
+		case 213 : mois = numeroJour <= (31*4)+(30*2)+29; break;
+		case 243 : mois = numeroJour <= (31*4)+(30*3)+29; break;
+		case 274 : mois = numeroJour <= (31*5)+(30*3+29); break;
+		case 304: mois = numeroJour <= (31*5)+(30*4)+29; break;
+		case 335: mois = numeroJour <= (31*6)+(30*4)+29; break;
+		case 365: mois = numeroJour <= (31*6)+(30*5)+29; break;
+		default: System.out.println("veuillez taper un chiffre"); break;
+		}*/
+		
+		//System.out.println("nous somme le :" + dateBuilder);
+	//System.out.println(numeroJour);
+		
+		
+		
 	}
 	
-	public static void ternaire_imbrique()
-	{
-		// condition ternaire imbriqué 
-		String resultat2 = (age < 18) ?
-				(age > 3) ?
-						"tu n'est pas enfant" : "tu est un enfant" 
-					: "tu est un adulte";
-		System.out.print (" " + resultat2);
-		// fin
-	}
 }
